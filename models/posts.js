729fec6a98be912fe03e5ca97a69f3abc0499bb0
Justin Schreiber <jschreiber234@gmail.com>
2021-07-08 16:12:35 -0400

    added mogoose models

---
+++ models/posts.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const PostSchema = new mongoose.Schema({
+    userID:{
+        type : Number,
+        required : true
+    },
+    date:{
+        type : Date,
+        default : Date.now
+    },
+    content:{
+        type: String,
+        required: true
+    },
+    replies: {
+        type: []
+    },
+    uri: {
+        type: String,
+        required: true
+    }
+
+})
+
+const Post = mongoose.model('Post',PostSchema);
+
+module.exports = Post;
