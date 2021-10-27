b75569f896a3d847c7d85e90196ee257ec5c5614
Justin Schreiber <jschreiber234@gmail.com>
2021-10-26 22:17:49 -0400

    changes

--- public/js/searchLoad.js
+++ public/js/searchLoad.js
@@ -1,6 +1,7 @@
-window.onload = function(){
-    search();
+window.onload = function () {
+  search();
 };
+
 function search() {
   var s = document.getElementById("search");
   s.addEventListener("keyup", function (event) {
@@ -9,11 +10,28 @@ function search() {
     }
   });
 
-  document.getElementById("search-btn").addEventListener("click", function (){
-      searching();
+  document.getElementById("search-btn").addEventListener("click", function () {
+    searching();
   });
 }
 
-function searching(){
-    console.log(document.getElementById("search").value);
+function searching() {
+  var value = document.getElementById("search").value;
+  if (value) {
+    //use for template  
+    var node = document.createElement("P");
+    var textNode = document.createTextNode(value);
+    node.setAttribute("tag", "result");
+    node.appendChild(textNode);
+    document.getElementById("results").appendChild(node);
+
+    node = document.createElement("P");
+    textNode = document.createTextNode(value);
+    node.setAttribute("tag", "result");
+    var n = document.createElement("a");
+    n.setAttribute("href","#");
+    n.appendChild(textNode);
+    node.appendChild(n);
+    document.getElementById("results").appendChild(node);
+  }
 }
