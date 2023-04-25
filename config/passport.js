dc88cef772e1595990553f00715c250792bbf71b
Justin Schreiber <jschreiber234@gmail.com>
2023-04-25 19:13:12 -0400

    removed comment

--- config/passport.js
+++ config/passport.js
@@ -33,7 +33,7 @@ module.exports = function(passport) {
     });
   
     passport.deserializeUser(async function(id, done) {
-      const user = await User.findById(id);//this needs to be fixed
+      const user = await User.findById(id);
       done(null, user);
     });
   };
