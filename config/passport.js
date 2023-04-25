b9dcbb4d4cdaa742f4f217674c08df4298705e5c
Justin Schreiber <jschreiber234@gmail.com>
2023-04-25 19:02:12 -0400

    fixed bugs in login and dashboard cause mongoose

--- config/passport.js
+++ config/passport.js
@@ -4,37 +4,36 @@ const bcrypt = require('bcryptjs');
 
 const User = require('../models/user');
 
+
+
 module.exports = function(passport) {
+    
     passport.use(
-      new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+      new LocalStrategy({ usernameField: 'email' },  async (email, password, done) => {
      
-        User.findOne({
-          email: email
-        }).then(user => {
-          if (!user) {
-            return done(null, false, { message: 'That email is not registered' });
+        const user = await User.find({ email: email });
+        if (!user) {
+          return done(null, false, { message: 'That email is not registered' });
+        }
+
+        bcrypt.compare(password, user.password, (isMatch) => {
+          if (isMatch) {
+            return done(null, user);
+          } else {
+            return done(null, false, { message: 'Password incorrect' });
           }
-  
-      
-          bcrypt.compare(password, user.password, (err, isMatch) => {
-            if (err) throw err;
-            if (isMatch) {
-              return done(null, user);
-            } else {
-              return done(null, false, { message: 'Password incorrect' });
-            }
-          });
         });
+
+
       })
     );
   
     passport.serializeUser(function(user, done) {
-      done(null, user.id);
+      done(null, user[0].id);
     });
   
-    passport.deserializeUser(function(id, done) {
-      User.findById(id, function(err, user) {
-        done(err, user);
-      });
+    passport.deserializeUser(async function(id, done) {
+      const user = await User.findById(id);//this needs to be fixed
+      done(null, user);
     });
   };
--- routes/index.js
+++ routes/index.js
@@ -1,54 +1,50 @@
 const express = require("express");
 const router = express.Router();
-const {ensureAuthenticated} = require("../config/auth");
-const {ensureAuthenticatedHome} = require("../config/authhome");
-const {ensureAuthenticatedProfile} = require("../config/authprofile");
+const { ensureAuthenticated } = require("../config/auth");
+const { ensureAuthenticatedHome } = require("../config/authhome");
+const { ensureAuthenticatedProfile } = require("../config/authprofile");
 const Post = require("../models/posts");
 const User = require("../models/user");
 
 router.get("/", ensureAuthenticatedHome, (req, res) => res.redirect("/dashboard"));
 
-router.get("/templateDashboard", ensureAuthenticated, (req, res) => res.render("templateDashboard", {name: req.user.name}));
+router.get("/templateDashboard", ensureAuthenticated, (req, res) => res.render("templateDashboard", { name: req.user.name }));
 
 router.get("/PostCreator", ensureAuthenticated, (req, res) => res.render("createPost"));
 
 router.get("/dashboard", ensureAuthenticated, (req, res) => res.render("dashboard"));
 
 //API Routes
-router.get("/api/:username/posts", function (req, res) {
-  User.findOne({
-    name: req.params.username
-  }, function (err, users) {
-    if (err) 
-      return console.error(err);
-    
-    if (users) {
-      Post.find({
-        userID: users._id
-      }, function (err2, posts) {
-        if (err2) 
-          return console.error(err2);
-        const result = posts.map(({content, date}) => {
-          return {name: users.name, content: content, date: date};
-        });
-        res.json(result);
-      }).sort("-date");
-    } else {
+router.get("/api/:username/posts", async function (req, res) {
+
+  const users = await Post.find({ name: req.params.username });
+
+  if (!users) console.log("could not find user")
+  else {
+    const posts = Post.find({ userID: users._id }).sort("-date");
+
+    if (posts){
+      const result = posts.map(({ content, date }) => {
+        return { name: users.name, content: content, date: date };
+      });
+      res.json(result);
+    }
+    else {
       res.sendStatus(500);
     }
-  });
+  }
 });
 
 router.get("/api/:username", function (req, res) {
   User.findOne({
     name: req.params.username
   }, function (err, users) {
-    if (err) 
+    if (err)
       return console.error(err);
-    
+
     if (users) {
       const result = user => {
-        return {name: user.name, following: user.following, followers: user.followers};
+        return { name: user.name, following: user.following, followers: user.followers };
       };
       res.json(result(users));
     } else {
@@ -62,13 +58,13 @@ router.get("/api/isFollowing/:username", function (req, res) {
     User.findOne({
       name: req.params.username
     }, function (err, users) {
-      if (err) 
+      if (err)
         return console.error(err);
       if (users) {
         if (req.user.following.includes(users.id)) {
-          res.json({isFollowing: true});
+          res.json({ isFollowing: true });
         } else {
-          res.json({isFollowing: false});
+          res.json({ isFollowing: false });
         }
       }
     });
@@ -78,37 +74,37 @@ router.get("/api/isFollowing/:username", function (req, res) {
 });
 router.get("/api/user/isAuthenticated", function (req, res) {
   if (req.user) {
-    res.json({isAuthenticated: true});
+    res.json({ isAuthenticated: true });
   } else {
-    res.json({isAuthenticated: false});
+    res.json({ isAuthenticated: false });
   }
 });
 
-router.post("/api/user/follow/", ensureAuthenticated, function(req,res) {
-  
-  var username =req.body.username;
-  var newFollower ="";
+router.post("/api/user/follow/", ensureAuthenticated, function (req, res) {
+
+  let username = req.body.username;
+  let newFollower = "";
   User.findOne({
     name: username
   }, function (err, users) {
-    if (err) 
+    if (err)
       return console.error(err);
-    
+
     if (users) {
-     newFollower = users.id;
-     User.updateOne({name: req.user.name},{ $addToSet: {following: [newFollower]}}, function(err2, result){
-      if (err2) {
-        res.send(err2);
-      } 
-     });
-     User.updateOne({name: users.name},{ $addToSet: {followers: [req.user.id]}}, function(err3, result){
-      if (err3) {
-        res.send(err3);
-      }  
-      else {
-        console.log(result);
-      }
-     });
+      newFollower = users.id;
+      User.updateOne({ name: req.user.name }, { $addToSet: { following: [newFollower] } }, function (err2, result) {
+        if (err2) {
+          res.send(err2);
+        }
+      });
+      User.updateOne({ name: users.name }, { $addToSet: { followers: [req.user.id] } }, function (err3, result) {
+        if (err3) {
+          res.send(err3);
+        }
+        else {
+          console.log(result);
+        }
+      });
     } else {
       res.sendStatus(500);
       return;
@@ -116,34 +112,34 @@ router.post("/api/user/follow/", ensureAuthenticated, function(req,res) {
 
     res.sendStatus(200);
   });
-  
+
 });
 
-router.post("/api/user/unfollow/", ensureAuthenticated, function(req,res) {
-  
-  var username =req.body.username;
-  var follower ="";
+router.post("/api/user/unfollow/", ensureAuthenticated, function (req, res) {
+
+  let username = req.body.username;
+  let follower = "";
   User.findOne({
     name: username
   }, function (err, users) {
-    if (err) 
+    if (err)
       return console.error(err);
-    
+
     if (users) {
-     follower = users.id;
-     User.updateOne({name: req.user.name},{ $pullAll : {following: [follower]}}, function(err4, result){
-      if (err4) {
-        res.send(err4);
-      } 
-     });
-     User.updateOne({name: users.name},{ $pullAll : {followers: [req.user.id]}}, function(err5, result){
-      if (err5) {
-        res.send(err5);
-      }  
-      else {
-        console.log(result);
-      }
-     });
+      follower = users.id;
+      User.updateOne({ name: req.user.name }, { $pullAll: { following: [follower] } }, function (err4, result) {
+        if (err4) {
+          res.send(err4);
+        }
+      });
+      User.updateOne({ name: users.name }, { $pullAll: { followers: [req.user.id] } }, function (err5, result) {
+        if (err5) {
+          res.send(err5);
+        }
+        else {
+          console.log(result);
+        }
+      });
     } else {
       res.sendStatus(500);
       return;
@@ -151,38 +147,32 @@ router.post("/api/user/unfollow/", ensureAuthenticated, function(req,res) {
 
     res.sendStatus(200);
   });
-  
-});
-
-router.get("/api/post/postDashboard", ensureAuthenticated, function(req, res){
-  var followedUserIDs = req.user.following;
-  Post.find({
-    userID: followedUserIDs
-  }, function (err5, posts) {
-    if (err5) 
-      return console.error(err5);
-
-    const post_result = posts.map(({userID, date, content}) => {
-      return {name: userID.name, date: date, content: content};
-    });
-
-    res.send(post_result);
-       
-  }).populate('userID').sort("-date");
 
 });
 
-router.get("/api/search/:username",function(req, res){
+router.get("/api/post/postDashboard", ensureAuthenticated,async function (req, res) {
+  let followedUserIDs = req.user.following;
+  const posts = await Post.find( {userID: followedUserIDs} ).populate('userID').sort("-date");
+  const post_result = posts.map(({ userID, date, content }) => {
+    return { name: userID.name, date: date, content: content };
+  });
+
+  res.send(post_result);
+
+
+});
+
+router.get("/api/search/:username", function (req, res) {
   User.find({
-    name: {$regex: req.params.username}
+    name: { $regex: req.params.username }
   }, function (errr, users) {
     if (errr)
       return console.log(errr);
     if (users) {
-      const result = users.map(({name}) => {
-        return {name: name};
+      const result = users.map(({ name }) => {
+        return { name: name };
       });
-      
+
       res.json(result);
     }
     else {
@@ -191,18 +181,18 @@ router.get("/api/search/:username",function(req, res){
   });
 });
 
-router.get("/search",(req, res) =>
-res.render("userSearch"));
+router.get("/search", (req, res) =>
+  res.render("userSearch"));
 
 
 //Profile Routes
 router.get("/:username/", (req, res) =>
-//check if valid username or give error page
-res.render("profile"));
+  //check if valid username or give error page
+  res.render("profile"));
 
 router.get("/:username/post/:postID", (req, res) =>
-//check if valid username and valid post id
-res.send("username: " + req.params.username + " postID: " + req.params.postID));
+  //check if valid username and valid post id
+  res.send("username: " + req.params.username + " postID: " + req.params.postID));
 
 
 //TODO create link to users displayed in posts and ability to search and follow from search and hide follow on own account
